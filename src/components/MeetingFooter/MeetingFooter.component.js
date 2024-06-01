d22eb904ed196955d689d1aaa60668a11e721994
Anthony Chilan <anthony.chilanp@gmail.com>
2024-06-01 12:19:15 -0500

    feat: Add screen sharing functionality to MeetingFooter component

--- src/components/MeetingFooter/MeetingFooter.component.js
+++ src/components/MeetingFooter/MeetingFooter.component.js
@@ -152,6 +152,7 @@ const MeetingFooter = (props) => {
           title="Mute"
         />
       </div>
+      <div className={"meeting-icons " + (streamState.screen ? "active" : "")} data-tip={streamState.screen ? "Stop Sharing" : "Start Sharing"} onClick={onScreenClick}>
       <div
         className={"meeting-icons " + (!streamState.video ? "active" : "")}
         data-tip={streamState.video ? "Hide Video" : "Show Video"}
